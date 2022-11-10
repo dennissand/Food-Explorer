d9c72a1e94f2f34e10b86e3ccf65638cb42a26e8
Dennis Sand <97163222+dennissand@users.noreply.github.com>
2022-11-10 11:01:02 +0100

    Initial Commit

---
+++ Food Explorer/Food_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_ExplorerApp.swift
+//  Food Explorer
+//
+//  Created by Dennis Sand on 10.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Food_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
